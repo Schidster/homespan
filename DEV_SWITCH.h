530a4be8ed32691cd8806740769f378644803e7f
Siddarth S <schidsteradinson@gmail.com>
2024-09-25 02:59:23 +0530

    Added Light and Fan

---
+++ DEV_SWITCH.h
@@ -0,0 +1,20 @@
+struct DEV_SWITCH : Service::Switch {
+
+  int ledPin;
+  SpanCharacteristic* power;
+
+  DEV_SWITCH(int ledPin) : Service::Switch() {
+
+    power = new Characteristic::On();
+    this->ledPin = ledPin;
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, HIGH);
+  }
+
+  boolean update() {
+
+    digitalWrite(ledPin, (int)!(power->getNewVal<bool>()));
+
+    return(true);
+  }
+};
